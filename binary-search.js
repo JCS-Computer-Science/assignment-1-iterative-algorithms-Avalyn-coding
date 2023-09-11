72e36ab04770fdc6239aaa24f7b3e9527e9ac28e
Carter Fox <16277010@edu.cbe.ab.ca>
2023-09-11 10:22:41 -0600

    edited linear and almost done binary

--- binary-search.js
+++ binary-search.js
@@ -1,4 +1,24 @@
 function binarySearch(array, searchTerm) {
+
+	let left = 0
+	let right = list.length
+	let mid = math.floor((left + right) /2);
+	
+	while(list[mid] !== val && left <= right) {
+		if (val < list [mid]){
+			right = mid - 1
+		}else{
+			left = mid + 1
+		}
+		mid = math.floor ((left + right) /2);
+
+	}
+	if (list [mid] === val) {
+		return mid;
+	}else{
+		return -1
+	}
+
 	/*
 	 ** Check the middle element, if it's higher than the searchTerm
 	 ** then check the element in between the start and the middle, otherwise
--- linear-search.js
+++ linear-search.js
@@ -1,12 +1,38 @@
 function linearSearch(array, searchTerm) {
-	/*
-	 ** Check each element from start to finish
-	 ** until you find an element that matches the search term
-	 **
-	 ** Return the index of the found element
-	 */
-
-	return null;
+	n = array.length 
+	for (i = 0; i ,n; i++){
+		if (array[i] == searchTerm)
+		return i;
+	}
+	return -1;
 }
+array = [2, 0 ,1 ,4, 9] 
+searchTerm = 1
+
+	result = linearSearch(array, searchTerm)
+
+	if (result == -1){
+	console.log("element not found");
+}else{
+	console.log("element found in index" + result);
+}
+/*
+** Check each element from start to finish
+** until you find an element that matches the search term
+**
+** Return the index of the found element
+*/
+// 	let linearSearch = (array, searchTerm)=>{
+// 		for (let i = 0; i < array.length; i++){
+// 			if(list[i] = searchTerm){
+// 				return i;
+// 			}
+// 		}
+// 		return -1
+// 	}
+
+// var array = [12, 41, 64, 14, 74, 85, 34, 26]
+// var searchTerm = [100]
+
 
 module.exports = linearSearch;
